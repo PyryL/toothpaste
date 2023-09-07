106f1fc43c0254d5b4e25751de1ba97fd7af849d
Pyry Lahtinen <pyry@pyry.info>
2023-09-07 18:26:54 +0300

    added page for reading paste

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1>ToothPaste</h1>"
+
+@app.route("/paste/<string:id>")
+def readPaste(id):
+    return render_template("paste.html",
+        fieldsDisabled="disabled",
+        pasteTitle=f"Paste called {id}",
+        pasteContent=f"This is the content of paste with ID {id}.",)
