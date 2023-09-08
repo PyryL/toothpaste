c889d0eabb7cc12916c208db3fe1853945ad98ef
Pyry Lahtinen <pyry@pyry.info>
2023-09-08 09:59:44 +0300

    new paste creation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 def index():
     return "<h1>ToothPaste</h1>"
 
-@app.route("/paste/<string:id>")
+@app.route("/paste/<string:id>", methods=["GET"])
 def readPaste(id):
     sql = "SELECT title, content FROM pastes WHERE id=:id"
     result = db.session.execute(text(sql), { "id": id })
@@ -19,6 +19,24 @@ def readPaste(id):
     paste = result.fetchone()
 
     return render_template("paste.html",
+        header="Read paste",
         fieldsDisabled="disabled",
         pasteTitle=paste.title,
         pasteContent=paste.content,)
+
+@app.route("/paste", methods=["POST"])
+def pastesPost():
+    sql = "INSERT INTO pastes (title, content) VALUES (:title, :content) RETURNING id"
+    values = { "title": request.form["title"], "content": request.form["content"] }
+    result = db.session.execute(text(sql), values)
+    db.session.commit()
+    new_id = result.fetchone().id
+    return redirect(f"/paste/{new_id}")
+
+@app.route("/new-paste")
+def newPaste():
+    return render_template("paste.html",
+        header="New paste",
+        fieldsDisabled="",
+        pasteTitle="",
+        pasteContent="",)
