f71ba2e7536585e82c1d15a654f57b67aa6aad46
Pyry Lahtinen <pyry@pyry.info>
2023-09-08 19:22:16 +0300

    extracted user database code to separate file

--- routes/user.py
+++ routes/user.py
@@ -1,6 +1,6 @@
-from app import app, db
+from app import app
 from flask import render_template, redirect, request, session
-from sqlalchemy import text
+from repositories.users import validate_credentials, add_new_user
 
 @app.route("/log-in", methods=["GET"])
 def getLogIn():
@@ -11,16 +11,10 @@ def getLogIn():
 
 @app.route("/log-in", methods=["POST"])
 def postLogIn():
-    # check credentials
-    sql = "SELECT id AS userid FROM users WHERE username=:username AND password_hash=crypt(:password, password_hash)"
-    parameters = { "username": request.form["username"], "password": request.form["password"] }
-    result = db.session.execute(text(sql), parameters)
-
-    # incorrect username and/or password
-    if result.rowcount != 1:
+    userId = validate_credentials(request.form["username"], request.form["password"])
+    if userId is None:
         return redirect("/log-in")
-
-    session["userid"] = result.fetchone().userid
+    session["userid"] = userId
     return redirect("/")
 
 @app.route("/sign-up", methods=["GET"])
@@ -40,11 +34,6 @@ def postSignUp():
         return "criteria not met"
     
     # add user to database
-    sql = "INSERT INTO users (username, password_hash) VALUES (:username, crypt(:password, gen_salt('bf'))) RETURNING id"
-    values = { "username": request.form["username"], "password": request.form["password1"] }
-    result = db.session.execute(text(sql), values)
-    db.session.commit()
-
-    # log user in and redirect to the front page
-    session["userid"] = result.fetchone().id
+    userid = add_new_user(request.form["username"], request.form["password1"])
+    session["userid"] = userid
     return redirect("/")
