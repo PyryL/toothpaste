e822e05af11050f4704d64947beda6b00be16033
Pyry Lahtinen <pyry@pyry.info>
2023-09-08 14:55:58 +0300

    added user sign up

--- app.py
+++ app.py
@@ -65,3 +65,29 @@ def postLogIn():
 
     session["userid"] = result.fetchone().userid
     return redirect("/")
+
+@app.route("/sign-up", methods=["GET"])
+def getSignUp():
+    # if user has already logged in, redirect to the front page
+    if "userid" in session:
+        return redirect("/")
+    return render_template("signup.html")
+
+@app.route("/sign-up", methods=["POST"])
+def postSignUp():
+    # check that given credentials meet requirements
+    # TODO: add more criteria for security
+    if request.form["password1"] != request.form["password2"] or \
+        len(request.form["password1"]) < 1 or len(request.form["password1"]) > 72 or \
+        len(request.form["username"]) < 1 or len(request.form["username"]) > 12:
+        return "criteria not met"
+    
+    # add user to database
+    sql = "INSERT INTO users (username, password_hash) VALUES (:username, crypt(:password, gen_salt('bf'))) RETURNING id"
+    values = { "username": request.form["username"], "password": request.form["password1"] }
+    result = db.session.execute(text(sql), values)
+    db.session.commit()
+
+    # log user in and redirect to the front page
+    session["userid"] = result.fetchone().id
+    return redirect("/")
