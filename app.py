29810d8bf0726f43ea7cfdce235e5cf724685559
Pyry Lahtinen <pyry@pyry.info>
2023-09-08 17:29:04 +0300

    paste can be set to private

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    sql = "SELECT id, title FROM pastes ORDER BY modification_date DESC LIMIT 10"
+    sql = "SELECT id, title FROM pastes WHERE publicity='listed' ORDER BY modification_date DESC LIMIT 10"
     pastes = db.session.execute(text(sql)).fetchall()
     return render_template("frontpage.html", pastes=pastes)
 
 @app.route("/paste/<string:id>", methods=["GET"])
 def readPaste(id):
-    sql = "SELECT title, content, owner FROM pastes WHERE id=:id"
+    sql = "SELECT title, content, owner, publicity FROM pastes WHERE id=:id"
     result = db.session.execute(text(sql), { "id": id })
     if result.rowcount != 1:
         return "404 paste not found"
@@ -27,11 +27,12 @@ def readPaste(id):
         header="Read paste",
         existingId=id,
         fieldsDisabled="" if has_edit_permission else "disabled",
+        pastePublicity=paste.publicity,
         pasteTitle=paste.title,
         pasteContent=paste.content,)
 
 @app.route("/paste", methods=["POST"])
-def pastesPost():
+def pastePost():
     # if this is editing, check that user has edit permissions
     if request.form["existingId"] != "":
         if "userid" not in session:
@@ -42,17 +43,34 @@ def pastesPost():
             return "404 invalid paste id"
         if result.fetchone().owner != session["userid"]:
             return "403 you are not the owner"
-
-    sql = "INSERT INTO pastes (title, content, owner) VALUES (:title, :content, :owner) RETURNING id"
-    values = {
-        "title": request.form["title"],
-        "content": request.form["content"],
-        "owner": session["userid"] if "userid" in session else None
-    }
+        
+        sql = """
+            UPDATE pastes SET title=:title, content=:content, publicity=:publicity
+            WHERE id=:pasteid
+            RETURNING id
+        """
+        values = {
+            "title": request.form["title"],
+            "content": request.form["content"],
+            "publicity": request.form["publicity"],
+            "pasteid": request.form["existingId"]
+        }
+    else:
+        sql = """
+            INSERT INTO pastes (title, content, owner, publicity)
+            VALUES (:title, :content, :owner, :publicity)
+            RETURNING id
+        """
+        values = {
+            "title": request.form["title"],
+            "content": request.form["content"],
+            "owner": session["userid"] if "userid" in session else None,
+            "publicity": request.form["publicity"]
+        }
     result = db.session.execute(text(sql), values)
     db.session.commit()
-    new_id = result.fetchone().id
-    return redirect(f"/paste/{new_id}")
+    pasteId = result.fetchone().id
+    return redirect(f"/paste/{pasteId}")
 
 @app.route("/new-paste")
 def newPaste():
--- docs/schema.sql
+++ docs/schema.sql
@@ -11,20 +11,23 @@ CREATE TABLE Users (
     password_hash TEXT NOT NULL
 );
 
+CREATE TYPE Publicity AS ENUM ('listed', 'private');
+
 CREATE TABLE Pastes (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
     modification_date TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    owner INTEGER REFERENCES Users
+    owner INTEGER REFERENCES Users,
+    publicity Publicity NOT NULL
 );
 
 
 -- test data
 
-INSERT INTO pastes (title, content) VALUES ('My first paste!', 'This is my first paste.');
-INSERT INTO pastes (title, content) VALUES ('Most useful tool', 'I found this mindblowing tool: console.log');
-INSERT INTO pastes (title, content) VALUES ('Instructions', 'Initialize your database with ./docs/database.sh');
+INSERT INTO pastes (title, content, publicity) VALUES ('My first paste!', 'This is my first paste.', 'listed');
+INSERT INTO pastes (title, content, publicity) VALUES ('Most useful tool', 'I found this mindblowing tool: console.log', 'listed');
+INSERT INTO pastes (title, content, publicity) VALUES ('Instructions', 'Initialize your database with ./docs/database.sh', 'listed');
 
 INSERT INTO users (username, password_hash) VALUES ('johndoe', crypt('secret', gen_salt('bf')));
 INSERT INTO users (username, password_hash) VALUES ('janedoe', crypt('password', gen_salt('bf')));
