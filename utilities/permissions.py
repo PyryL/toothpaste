cf0da6e68e1711363c03c67c27eaa5ebfba5b938
Pyry Lahtinen <pyry@pyry.info>
2023-09-19 13:26:25 +0300

    fixed bug in token regeneration permission

--- utilities/permissions.py
+++ utilities/permissions.py
@@ -26,7 +26,7 @@ class Permissions:
     def can_regenerate_tokens(cls, token_level: str, paste_owner_id: int, logged_in_user_id: int) -> bool:
         # modify-level token is always required
         # in addition, user must be paste owner if the paste was not created anonymously
-        return token_level == "modify" and paste_owner_id == logged_in_user_id
+        return token_level == "modify" and (paste_owner_id is None or paste_owner_id == logged_in_user_id)
 
     @classmethod
     def can_vote(cls, paste_publicity: str, paste_owner_id: int, logged_in_user_id: int) -> bool:
