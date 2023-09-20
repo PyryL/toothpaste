f01494ba80867c983ca979a88065e02beb471e38
Pyry Lahtinen <pyry@pyry.info>
2023-09-20 13:38:23 +0300

    enhanced frontpage

--- routes/index.py
+++ routes/index.py
@@ -1,16 +1,13 @@
 from app import app, db
 from flask import render_template
 from sqlalchemy.sql import text
-from utilities.session import is_user_logged_in
+from utilities.session import is_user_logged_in, get_logged_in_user_id
+from repositories import PasteRepository
 
 @app.route("/")
 def index():
-    sql = """
-        SELECT p.title AS title, t.token AS token
-        FROM pastes AS p, tokens AS t
-        WHERE t.paste=p.id AND t.level='view' AND p.publicity='listed' AND p.is_encrypted=FALSE
-        ORDER BY modification_date DESC
-        LIMIT 10
-    """
-    pastes = db.session.execute(text(sql)).fetchall()
-    return render_template("frontpage.html", isLoggedIn=is_user_logged_in(), pastes=pastes)
+    logged_in_user_id = get_logged_in_user_id()
+    return render_template("frontpage.html",
+        isLoggedIn=is_user_logged_in(),
+        latestPastes=PasteRepository.get_latest_frontpage_pastes(logged_in_user_id),
+        myPastes=PasteRepository.get_pastes_of_user(logged_in_user_id) if is_user_logged_in() else [])
