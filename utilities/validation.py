8871c0160030d3f3660dfc82c63f4901e1506f39
Pyry Lahtinen <pyry@pyry.info>
2023-09-29 12:02:42 +0300

    fixed a critical password validation bug

--- utilities/validation.py
+++ utilities/validation.py
@@ -7,12 +7,13 @@ class InputValidation:
 
     @classmethod
     def is_valid_password(cls, password: str) -> bool:
+        # list item is True, if password meets that exact criteria
         criteria = [
-            len(password) < 8,
-            len(password) > 72,
-            re.compile(r"[0-9]").search(password) is None,
-            re.compile(r"[a-z]").search(password) is None,
-            re.compile(r"[A-Z]").search(password) is None,
-            re.compile(r"[^a-zA-Z0-9]").search(password) is None
+            len(password) >= 8,
+            len(password) <= 72,
+            re.compile(r"[0-9]").search(password) is not None,
+            re.compile(r"[A-Z]").search(password) is not None,
+            re.compile(r"[a-z]").search(password) is not None,
+            re.compile(r"[^a-zA-Z0-9]").search(password) is not None,
         ]
         return False not in criteria
